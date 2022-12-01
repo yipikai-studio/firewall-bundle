3ad8926103d358db5519ad37967fcdef3ba3dab0
Matthieu Beurel <matthieu@mbeurel.com>
2022-12-01 09:52:17 +0100

    doc : fix doc file

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Yipikai Firewall Bundle package.
  *
- * (c) Austral <support@austral.dev>
+ * (c) Yipikai <support@yipikai.studio>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- Listener/FirewallListener.php
+++ Listener/FirewallListener.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Yipikai Firewall Bundle package.
  *
- * (c) Austral <support@yipikai.studio>
+ * (c) Yipikai <support@yipikai.studio>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -10,10 +10,12 @@
 
 namespace Yipikai\FirewallBundle\Listener;
 
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Yipikai\FirewallBundle\Configuration\FirewallConfiguration;
+use Yipikai\FirewallBundle\Event\FirewallEvent;
 use Yipikai\FirewallBundle\Services\Firewall;
 
 /**
@@ -34,13 +36,20 @@ class FirewallListener
   protected FirewallConfiguration $firewallConfiguration;
 
   /**
+   * @var EventDispatcherInterface|null
+   */
+  protected ?EventDispatcherInterface $dispatcher = null;
+
+  /**
    * @param Firewall $firewall
    * @param FirewallConfiguration $firewallConfiguration
+   * @param EventDispatcherInterface|null $dispatcher
    */
-  public function __construct(Firewall $firewall, FirewallConfiguration $firewallConfiguration)
+  public function __construct(Firewall $firewall, FirewallConfiguration $firewallConfiguration, ?EventDispatcherInterface $dispatcher)
   {
     $this->firewall = $firewall;
     $this->firewallConfiguration = $firewallConfiguration;
+    $this->dispatcher = $dispatcher;
   }
 
   /**
@@ -53,18 +62,22 @@ class FirewallListener
   {
     $checkAccess = false;
     $redirect = null;
+    $type = null;
     if($this->firewallConfiguration->get("filters.environnement.dev.enabled") === true && $event->getRequest()->server->get("APP_ENV") === "dev")
     {
+      $type = "env.dev";
       $checkAccess = true;
       $redirect = $this->firewallConfiguration->get("filters.environnement.dev.redirect");
     }
     elseif($this->firewallConfiguration->get("filters.environnement.prod.enabled") === true && $event->getRequest()->server->get("APP_ENV") === "prod")
     {
+      $type = "env.prod";
       $checkAccess = true;
       $redirect = $this->firewallConfiguration->get("filters.environnement.prod.redirect");
     }
     elseif($this->firewallConfiguration->get("filters.path.enabled") === true && ($paths = $this->firewallConfiguration->get("filters.path.list")))
     {
+      $type = "path";
       foreach($paths as $path)
       {
         if(strpos($event->getRequest()->getRequestUri(), $path) === 0)
@@ -74,10 +87,28 @@ class FirewallListener
         }
       }
     }
-    if($checkAccess) {
-      if(!$this->firewall->authorize($event->getRequest()))
+    $firewallEvent = new FirewallEvent();
+    if($checkAccess)
+    {
+      $firewallEvent->setType($type);
+      $firewallEvent->setIsEnabled(true);
+      if($this->dispatcher)
       {
-        $response = new RedirectResponse($redirect, 301);
+        $this->dispatcher->dispatch($firewallEvent, FirewallEvent::EVENT_YIPIKAI_FIREWALL_ENABLED);
+      }
+    }
+
+    if($firewallEvent->getIsEnabled()) {
+      $isAuthorize = $this->firewall->authorize($event->getRequest());
+      $firewallEvent->setIsAuthorize($isAuthorize);
+      $firewallEvent->setRedirect($redirect);
+      if($this->dispatcher)
+      {
+        $this->dispatcher->dispatch($firewallEvent, FirewallEvent::EVENT_YIPIKAI_FIREWALL_AUTHORIZE);
+      }
+      if(!$firewallEvent->getIsAuthorize())
+      {
+        $response = new RedirectResponse($firewallEvent->getRedirect(), 301);
         $event->setResponse($response);
       }
     }
